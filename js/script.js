8d958d93ca26895024f310c80633a8bdfba14997
alessiodalberto002 <aledalberto002.com>
2025-12-25 17:12:12 +0100

    Mostriamo i risultati

--- js/script.js
+++ js/script.js
@@ -12,15 +12,26 @@ function separaNumeri(array) {
             dispari.push(array[i]);
         }
     }
+
+
+    // CALCOLIAMO LE SOMME //
+    let sommaPari = 0;
+    for (let i = 0; i < pari.length; i++) {
+        sommaPari += pari[i];
+    }
+
+    let sommaDispari = 0;
+    for (let i = 0; i < dispari.length; i++) {
+        sommaDispari += dispari[i];
+    }
+
+
+    // MOSTRIAMO I RISULTATI //
+    console.log("Numeri pari:", pari);
+    console.log("Somma pari:", sommaPari);
+    console.log("\nNumeri dispari:", dispari);
+    console.log("Somma dispari:", sommaDispari);
+
+    return { pari, dispari, sommaPari, sommaDispari };
 }
 
-// CALCOLIAMO LE SOMME //
-let sommaPari = 0;
-for (let i = 0; i < pari.length; i++) {
-    sommaPari += pari[i];
-}
-
-let sommaDispari = 0;
-for (let i = 0; i < dispari.length; i++) {
-    sommaDispari += dispari[i];
-}
