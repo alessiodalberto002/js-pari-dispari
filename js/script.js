9e19edf305dfc789c47b0d3a832ad34c65f3793c
alessiodalberto002 <aledalberto002.com>
2025-12-25 17:11:08 +0100

    Calcoliamo le somme

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-function separaNumeri() {
-  // ARRAY PER NUMERI PARI E DISPARI //
-  const pari = [];
-  const dispari = [];
-  
-  // SCORRIAMO L'ARRAY //
-  for (let i = 0; i < array.length; i++) {
-    // SE IL NUMERO E' DIVISIBILE DUE E' PARI //
-    if (array[i] % 2 === 0) {
-      pari.push(array[i]);
-    } else {
-      dispari.push(array[i]);
+function separaNumeri(array) {
+    // ARRAY PER NUMERI PARI E DISPARI //
+    const pari = [];
+    const dispari = [];
+
+    // SCORRIAMO L'ARRAY //
+    for (let i = 0; i < array.length; i++) {
+        // SE IL NUMERO E' DIVISIBILE DUE E' PARI //
+        if (array[i] % 2 === 0) {
+            pari.push(array[i]);
+        } else {
+            dispari.push(array[i]);
+        }
     }
-  }
+}
+
+// CALCOLIAMO LE SOMME //
+let sommaPari = 0;
+for (let i = 0; i < pari.length; i++) {
+    sommaPari += pari[i];
+}
+
+let sommaDispari = 0;
+for (let i = 0; i < dispari.length; i++) {
+    sommaDispari += dispari[i];
 }
